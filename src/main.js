bb223509babccce34248f6654aef839ebf809cab
codesinging <codesinging@gmail.com>
2020-08-29 17:26:48 +0800

    release 2.0.3

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import '../dist/bootstrap-icons-font.css'
 
+import 'bootstrap'
+
 Vue.config.productionTip = false
 
 new Vue({
